6ce62b84367b5d95e106bb4a889c1757c9142bab
A1cey <mschelkle05@web.de>
2025-08-17 17:13:33 +0200

    [core] Fixed lib doc code to work

--- emulator_core/src/lib.rs
+++ emulator_core/src/lib.rs
@@ -2,19 +2,19 @@
 //!
 //! The design is loosely based on the ARM architecture.
 //!
-//! The processor operates by loading and executing an assembly [`Program`](program::Program). 
-//! A [`Program`](program::Program) is a collection of assembly instructions that the processor iterates over and executes. 
+//! The processor operates by loading and executing an assembly [`Program`](program::Program).
+//! A [`Program`](program::Program) is a collection of assembly instructions that the processor iterates over and executes.
 //! The instruction set in use must implement the [`InstructionSet`](instruction_set::InstructionSet) trait.
 //! A default instruction set is available via the [`assembler`](../assembler/index.html) crate.
 //!
 //! # Implementing Custom Instruction Sets
-//! 
+//!
 //! The crate provides a [`Word`](word::Word) trait.
-//! This trait wraps the underlying type used as the processor’s word size, mimicking real-world architectures 
+//! This trait wraps the underlying type used as the processor’s word size, mimicking real-world architectures
 //! (e.g., [`I32`](word::I32) corresponds to a 32-bit architecture).
-//! 
+//!
 //! The [`Registers`](register::Registers) and [`Stack`](stack::Stack) use [`Word`](word::Word) as their data type.
-//! 
+//!
 //! The following signed integer types implement the [`Word`](word::Word) trait:
 //! - [`I8`](word::I8)
 //! - [`I16`](word::I16)
@@ -22,16 +22,32 @@
 //! - [`I64`](word::I64)
 //! - [`I128`](word::I128)
 //! - [`ISize`](word::ISize)
-//! 
+//!
 //! These types use two's complement representation, mirroring how real-world processor architectures work.
-//! 
-//! The processor’s [`Registers`](register::Registers), [`Flags`](register::Flag) and [`Stack`](stack::Stack) 
+//!
+//! The processor’s [`Registers`](register::Registers), [`Flags`](register::Flag) and [`Stack`](stack::Stack)
 //! are directly accessible and modifiable through the [`Processor`](processor::Processor) structure.
-//! 
+//!
 //! ```
+//! # use emulator_core::register::{Flag, Register};
+//! # use emulator_core::processor::Processor;
+//! # use emulator_core::instruction_set::InstructionSet;
+//! # use emulator_core::word::{I32, Word};
+//! # use core::marker::PhantomData;
+//! #
+//! # #[derive(Debug, PartialEq, Eq, Clone, Hash)]
+//! # struct Instruction<const STACK_SIZE: usize, W: Word> (PhantomData<W>);
+//! # 
+//! # impl<const STACK_SIZE: usize, W: Word> InstructionSet<STACK_SIZE> for Instruction<STACK_SIZE, W> {
+//! #     type Instruction = Self;
+//! #     type W = W;
+//! #     fn execute(instruction: &Self, processor: &mut Processor<STACK_SIZE, Self>) {}
+//! # }
+//! #
+//! # let mut processor = Processor::<2048, Instruction<2048, I32>>::new();
 //! let r0 = processor.registers.get_reg(Register::R0);
 //! processor.registers.set_reg(Register::R1, r0);
-//! 
+//!
 //! let overflow = processor.registers.get_flag(Flag::V);
 //! ```
 
